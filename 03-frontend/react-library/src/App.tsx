0fa23577d3c19923f562a68d4c222e67fa12190b
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-09-15 07:40:50 -0400

    add carousel components and its child-components of ReturnBooks. Also adding Heros component.

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
+import { Heros } from './layouts/HomePage/Heros';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <ExploreTopBooks/>
+      <Carousel/>
+      <Heros/>
     </div>
     
   );
