c9767c93786815b65391e6c4a88b10b0d5a09d38
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-09-19 07:01:21 -0400

    fix img tag and add mobile version of displaying the image of book

--- 03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
+++ 03-frontend/react-library/src/layouts/SearchBooksPage/components/SearchBook.tsx
@@ -7,17 +7,32 @@ export const SearchBook: React.FC<{ book: BookModel }> = (props) => {
                 <div className='col-md-2'>
                     <div className='d-none d-lg-block'>
                         {props.book.img ?
-                            <img src={props.book.img}>
+                            <img src={props.book.img}
                                 width='123'
                                 height='196'
                                 alt='Book'
-                            </img>
+                            />
                             :
-                            <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}>
+                            <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
                                 width='123'
                                 height='196'
                                 alt='Book'
-                            </img>
+                            />
+                        }
+                    </div>
+                    <div className='d-lg-none d-flex justify-content-center align-items-center'>
+                        {props.book.img ?
+                            <img src={props.book.img}
+                                width='123'
+                                height='196'
+                                alt='Book'
+                            />
+                            :
+                            <img src={require('../../../Images/BooksImages/book-luv2code-1000.png')}
+                                width='123'
+                                height='196'
+                                alt='Book'
+                            />
                         }
                     </div>
                 </div>
