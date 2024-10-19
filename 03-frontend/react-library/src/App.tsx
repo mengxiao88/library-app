01aae1ebc861b89a31a87255afd4e0fc635fd222
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-10-19 07:43:39 -0400

    adding checkout page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -6,6 +6,7 @@ import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBook } from "./layouts/SearchBooksPage/components/SearchBook";
 import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 import { Redirect, Route, Switch } from "react-router-dom";
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
   return (
@@ -21,6 +22,9 @@ export const App = () => {
           </Route>
           <Route path="/search">
             <SearchBooksPage />
+          </Route>
+          <Route path="/checkout/:bookId">
+            <BookCheckoutPage />
           </Route>
         </Switch>
       </div>
