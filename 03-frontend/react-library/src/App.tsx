e944d6f3b394942f269a8b2f261e44ba3c67bb5d
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-10-19 07:06:30 -0400

    replace anchor with link and add css to keep footer consistent across all pages.

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { HomePage } from './layouts/HomePage/HomePage';
-import { SearchBook } from './layouts/SearchBooksPage/components/SearchBook';
-import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBook } from "./layouts/SearchBooksPage/components/SearchBook";
+import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom";
 
 export const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/home' />
-        </Route>
-        <Route path='/home'>
-          <HomePage />
-        </Route>
-        <Route path='/search'>
-          <SearchBooksPage />
-        </Route>
-      </Switch>
+      <div className="flex-grow-1">
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/home" />
+          </Route>
+          <Route path="/home">
+            <HomePage />
+          </Route>
+          <Route path="/search">
+            <SearchBooksPage />
+          </Route>
+        </Switch>
+      </div>
       <Footer />
     </div>
   );
-}
+};
