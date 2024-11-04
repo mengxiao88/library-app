c23e5019e7a79063af1e85c9c3e074b7c39f1a5c
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-11-04 06:46:39 -0500

    bookController

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/BookController.java
@@ -0,0 +1,25 @@
+package com.luv2code.spring_boot_library.controller;
+
+import com.luv2code.spring_boot_library.entity.Book;
+import com.luv2code.spring_boot_library.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:3000")
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    private BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PutMapping("/secure/checkout")
+    public Book checkoutBook(@RequestParam Long bookId) throws Exception {
+        String userEmail = "testuser@email.com";
+        return bookService.checkoutBook(userEmail, bookId);
+    }
+}
