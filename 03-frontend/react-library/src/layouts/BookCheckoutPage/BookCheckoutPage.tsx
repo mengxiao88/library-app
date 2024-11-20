564c9d5be292d1e6116ffc24a42812b30ce252a1
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-11-20 07:30:27 -0500

    add reviewRender component

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -19,6 +19,9 @@ export const BookCheckoutPage = () => {
   const [totalStars, setTotalStars] = useState(0);
   const [isLoadingReview, setIsLoadingReview] = useState(true);
 
+  const [isReviewLeft, setIsReviewLeft] = useState(false);
+  const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
+
   // Loans Count State
   const [currentLoansCount, setCurrentLoansCount] = useState(0);
   const [isLoadingCurrentLoansCount, setIsLoadingCurrentLoansCount] = useState(true);
@@ -103,7 +106,33 @@ export const BookCheckoutPage = () => {
       setHttpError(error.message);    
     })
 
-  }, []);
+  }, [isReviewLeft]);
+
+  useEffect(() => {
+    const fetchUserReviewBook = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url = `http://localhost:8080/api/reviews/secure/user/book?bookId=${bookId}`;
+        const requestOptions = {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+            'Content-Type': 'application/json'
+          }
+        };
+        const userReview = await fetch(url, requestOptions);
+        if (!userReview.ok) {
+          throw new Error('Something went wrong');
+        }
+        const userReviewResponseJson = await userReview.json();
+        setIsReviewLeft(userReviewResponseJson);
+      }
+      setIsLoadingUserReview(false);
+    }
+    fetchUserReviewBook().catch((error:any) => {
+      setIsLoadingUserReview(false);
+      setHttpError(error.message);
+    })
+  }, [authState]);
 
   useEffect(() =>{
     const fetchUserCurrentLoansCount = async () => {
@@ -160,7 +189,9 @@ export const BookCheckoutPage = () => {
   },[authState]);
 
 
-  if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut) {
+  if (isLoading || isLoadingReview || isLoadingCurrentLoansCount || isLoadingBookCheckedOut 
+      || isLoadingUserReview
+  ) {
     return <SpinnerLoading />;
   }
   if (httpError) {
@@ -213,7 +244,7 @@ export const BookCheckoutPage = () => {
           </div>
           <CheckoutAndReviewBox book={book} mobile={false} currentLoansCount={currentLoansCount}
                 isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut} 
-                checkoutBook={checkoutBook}/>
+                checkoutBook={checkoutBook} isReviewLeft={isReviewLeft}/>
         </div>
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={false} />
@@ -241,7 +272,7 @@ export const BookCheckoutPage = () => {
         </div>
         <CheckoutAndReviewBox book={book} mobile={true} currentLoansCount={currentLoansCount}
             isAuthenticated={authState?.isAuthenticated} isCheckedOut={isCheckedOut}
-            checkoutBook={checkoutBook}/>
+            checkoutBook={checkoutBook} isReviewLeft={isReviewLeft}/>
         <hr />
         <LatestReviews reviews={reviews} bookId={book?.id} mobile={true} />
       </div>
