8351b4ae6ed12fce918806e5f3e7fa48636506aa
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-11-25 07:53:10 -0500

    add loansModal component

--- 03-frontend/react-library/src/layouts/ShelfPage/components/Loans.tsx
+++ 03-frontend/react-library/src/layouts/ShelfPage/components/Loans.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
 import { SpinnerLoading } from "../../Utils/SpinnerLoading";
 import { Link } from "react-router-dom";
+import { LoansModal } from "./LoansModal";
 
 export const Loans = () => {
 
@@ -111,6 +112,7 @@ export const Loans = () => {
               </div>
             </div>
             <hr />
+            <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={false}/>
           </div>
         ))}
         </>
@@ -181,6 +183,7 @@ export const Loans = () => {
                 </div>
               </div>
             <hr />
+            <LoansModal shelfCurrentLoan={shelfCurrentLoan} mobile={true}/>
           </div>
         ))}
         </>
