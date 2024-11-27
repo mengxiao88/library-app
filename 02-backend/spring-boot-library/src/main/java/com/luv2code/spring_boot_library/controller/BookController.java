642487f302c55de219cd9050fea373425c156f86
mengxiao88 <3017087+mengxiao88@users.noreply.github.com>
2024-11-27 06:10:11 -0500

    return book service and controller

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/BookController.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/BookController.java
@@ -51,4 +51,11 @@ public class BookController {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "sub");
         return bookService.currentLoansCount(userEmail);
     }
+
+    @PutMapping("/secure/return")
+    public void returnBook(@RequestHeader(value="Authorization") String toke,
+                           @RequestParam Long bookId) throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(toke, "sub");
+        bookService.returnBook(userEmail, bookId);
+    }
 }
